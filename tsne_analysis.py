06d9499ce7c74021b4cf6699a8cd529e0b4450e4
kborziak <borziak@gmail.com>
2023-01-09 14:52:25 -0500

    Update tsne_analysis.py

--- tsne_analysis.py
+++ tsne_analysis.py
@@ -2,12 +2,9 @@ from __future__ import print_function
 import time
 import numpy as np
 import pandas as pd
-#from sklearn.datasets import fetch_mldata
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-##matplotlib inline
 import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 
 #Import data
@@ -18,22 +15,19 @@ gene_set = full_matrix.columns
 full_matrix['class'] = sample_class['class']
 full_matrix['label'] = full_matrix['class'].apply(lambda i: str(i))
 
-#df_subset = full_matrix[gene_set].values
-
 #t-SNE
 time_start = time.time()
 
+# Perform the t-SNE calculations
 tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
-#tsne_results = tsne.fit_transform(df_subset)
 tsne_results = tsne.fit_transform(full_matrix[gene_set].values)
 
 print('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
 
-#df_subset['tsne-2d-one'] = tsne_results[:,0]
-#df_subset['tsne-2d-two'] = tsne_results[:,1]
 full_matrix['tsne-2d-one'] = tsne_results[:,0]
 full_matrix['tsne-2d-two'] = tsne_results[:,1]
 
+# Plot t-SNE results
 plt.figure(figsize=(30,30))
 sns.scatterplot(
     x="tsne-2d-one", y="tsne-2d-two",
@@ -45,5 +39,6 @@ sns.scatterplot(
 )
 plt.savefig('data/liver CGC t-SNE 5 trim norm.png')
 
+# Save t-SNE output matrix
 tsne_matrix = full_matrix[['class','tsne-2d-one','tsne-2d-two']]
 tsne_matrix.to_csv(r'/data/liver_csc/data/merged_matrix_5_trim_norm_tsne_matrix.tsv', sep="\t")
